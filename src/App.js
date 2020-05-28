af6fc5dd5672d20e61b95930ad77ab55e81c2ca7
Alan Cleyton <alancleyton67@gmail.com>
2020-05-28 12:58:31 -0300

    Deploy teste no Netlify

--- src/App.js
+++ src/App.js
@@ -8,16 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Netlify
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>
+          Deploy Teste para o Netlify usando o Create React App
+        </p>
       </header>
     </div>
   );
